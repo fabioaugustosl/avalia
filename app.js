21dd46ecdabb915f11312eb9a5f655c4f3632d32
root <fabioaugustosl@gmail.com>
2016-09-08 19:41:15 -0300

    início projeto

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+	res.send('Hello world');
+});
+
+app.listen(5000, function(err){
+	console.log('running avalia');
+});
+
